05a64a7e096714565034c6d5d323a5d346d7dd0a
emptydream-zdb <2633069208@qq.com>
2023-04-23 01:24:56 +0800

    add some exegesis

--- http_client.py
+++ http_client.py
@@ -7,10 +7,10 @@ class js_data(object):
     def __init__(self, json_file):
         with open(json_file,"r") as fin:
             json_dict = js.load(fin)
-        self.method = json_dict["method"]
-        self.data = js.dumps(json_dict["data"])
-        self._rq: requests.request
-    def run(self, url):
+        self.method = json_dict["method"] # the method to request
+        self.data = js.dumps(json_dict["data"]) # the data to request
+        self._rq: requests.request # the response from the web
+    def run(self, url): # solve sending the request
         if self.method == "ADD":
             r = requests.post(url+"/device/add", data = self.data, timeout=0.001)
         elif self.method == "DELETE":
@@ -20,27 +20,27 @@ class js_data(object):
         elif self.method == "QUERYALL":
             r = requests.get(url+"/device/getall", timeout=0.001)
         self._rq = r
-    def get_response(self):
+    def get_response(self): # get the response json data
         return self._rq.json()
     def get_status(self):
-        return self._rq.status_code
+        return self._rq.status_code# get the response status code
     
 
 class http_client(object):
     
     def __init__(self, client_name: str,url_str:str):
-        self.name = client_name
-        self.request:js_data = None
-        self.url = url_str
+        self.name = client_name # the name of the open client
+        self.request:js_data = None # the rquest deal obj
+        self.url = url_str # the rquest url
 
-    def send(self,json_file):
+    def send(self,json_file): # send the request
         self.request = js_data(json_file)
         self.request.run(self.url)
     
-    def get_re_json(self):
+    def get_re_json(self): # return the response json data
         return self.request.get_response()
     
-    def get_re_code(self):
+    def get_re_code(self): # return the response status code
         return self.request.get_status()
 
 
@@ -53,11 +53,13 @@ if __name__ == "__main__":
     except Exception as e:
         print(f"input error: {e}")
     url = "http://"+server_ip+":"+server_port
-    http_connect = http_client(input_file,url_str=url)
+
+    http_connect = http_client(input_file,url_str=url) # create the connect
     http_connect.send(input_file)
-    if http_connect.get_re_code() != 200:
+
+    if http_connect.get_re_code() != 200: # deal the error about the incorrect connect
         output = {"status": "fail", "data": None}
     else:
-        output = http_connect.get_re_json()
+        output = http_connect.get_re_json() # deal the response from the web
     with open(output_file,"w") as fout:
         js.dump(output,fout)
--- http_server.py
+++ http_server.py
@@ -14,61 +14,81 @@ except Exception as e:
 
 app = Sanic("http_server")
 
-async def find_device(id_list, db_list) -> list:
+async def find_device(id_list: list, db_list: list) -> list:
+    """ return the finded device index in db_list
+    :parm: id_list: list   the list of the device or the id to find
+           db_list: list   the list of the device in db 
+    """
     res_list = []
     for device in id_list:
-        if type(device) == dict:
+        if type(device) == dict: # judge element in id_list is id or device
             dev_id = device["id"]
         else:
             dev_id = device
         for x in range(len(db_list)):
-            if str(db_list[x]["id"]) == str(dev_id):
+            if str(db_list[x]["id"]) == str(dev_id): # find the index and insert the res_list
                 res_list.append(x)
                 break
     return res_list
 
 @app.get("/device/getall", name="/device/getall")
 async def get_all_device(request):
+    """solve the getall request
+       method: get
+       response: json  The all device data
+    """
     response = {"status": "success","data": cv.csv_to_js(db_path)}
     return rp.json(response)
 
 
 @app.post("/device/add", name="device/add")
 async def add_device(request):
+    """solve the add request
+       method: post
+       response: json  The status of the resul: sucess or fail
+    """
     re_info = request.json
-    dataList = cv.csv_to_js(db_path)
-    finded_index = await find_device(re_info, dataList)
-    if len(finded_index) == 0:
+    dataList = cv.csv_to_js(db_path) # read db_data as a list
+    finded_index = await find_device(re_info, dataList) 
+    if len(finded_index) == 0: # the device to add is not in db 
         cv.js_to_csv(re_info,"db.csv",mode='a')
         result = {"status": "success", "data": None}
-    else:
+    else: # the device to add is in db 
         result = {"status": "fail", "data": None}
     return rp.json(result)
 
 
 @app.post("/device/del", name="device/del")
 async def del_device(request):
+    """solve the del request
+       method: post
+       response: json  The status of the resul: sucess or fail
+    """
     re_info = request.json
-    dataList = cv.csv_to_js(db_path)
+    dataList = cv.csv_to_js(db_path)# read db_data as a list
     finded_index = await find_device(re_info, dataList)
-    if len(finded_index) != 0:
+    if len(finded_index) != 0: # delate the device
         for x in reversed(finded_index):
             dataList.pop(x)
         cv.js_to_csv(dataList,"db.csv",mode='w')
         result = {"status": "success", "data": None}
-    else:
+    else: # db does not have the device to del
         result = {"status": "fail", "data": None}
     return rp.json(result)
 
 
 @app.post("/device/get", name="device/get")
 async def get_device(request):
+    """solve the get request
+       method: post
+       response: json  The status of the resul: sucess or fail ,and the device data which to get
+    """
     re_info = request.json
-    dataList = cv.csv_to_js(db_path)
+    dataList = cv.csv_to_js(db_path)# read db_data as a list
     finded_index = await find_device(re_info, dataList)
     if len(finded_index) != 0:
         result = {"status": "success", "data":[dataList[x] for x in finded_index]}
-    else:
+    else: # db does not have the device to get
         result = {"status": "fail", "data": None}
     return rp.json(result)
 
